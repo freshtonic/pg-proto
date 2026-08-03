0952d40cf89ad4b5bb74aefd94629bfce68575e8
James Sadler <freshtonic@gmail.com>
2026-08-03 22:49:48 +1000

    generate startup and authentication grammars

--- src/grammar.rs
+++ src/grammar.rs
@@ -62,9 +62,72 @@ protocol! {
     }
 }
 
+protocol! {
+    pub mod pre_startup {
+        initial PreStartup;
+        PreStartup {
+            SslRequest(ssl_request) => AwaitingSslReply,
+            GssRequest(gss_request) => AwaitingGssReply,
+            Cancel(cancel) => Terminated,
+            Startup(startup) => Auth,
+        }
+        AwaitingSslReply {
+            Accept(accept) => TlsHandshake,
+            Reject(reject) => PreStartup,
+            LegacyError(legacy_error) => Terminated,
+        }
+        AwaitingGssReply {
+            Accept(accept) => GssHandshake,
+            Reject(reject) => PreStartup,
+            LegacyError(legacy_error) => Terminated,
+        }
+        TlsHandshake {
+            HandshakeComplete(complete) => PreStartup,
+        }
+        GssHandshake {
+            HandshakeComplete(complete) => PreStartup,
+        }
+        Auth {}
+        Terminated {}
+    }
+}
+
+protocol! {
+    pub mod authentication {
+        initial Auth;
+        Auth {
+            Ok(ok) => AwaitingStartupReady,
+            Cleartext(cleartext) => PasswordResponse,
+            Md5(md5) => PasswordResponse,
+            Sasl(sasl) => SaslInitial,
+            Gss(gss) => Auth,
+            Sspi(sspi) => Auth,
+            KerberosV5(kerberos_v5) => Auth,
+        }
+        PasswordResponse {
+            Password(password) => AwaitingAuthOk,
+        }
+        SaslInitial {
+            Initial(initial) => Sasl,
+        }
+        Sasl {
+            Continue(continue_response) => Sasl,
+            Final(final_response) => AwaitingAuthOk,
+        }
+        AwaitingAuthOk {
+            Ok(ok) => AwaitingStartupReady,
+        }
+        AwaitingStartupReady {
+            Ready(ready) => Ready,
+        }
+        Ready {}
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::frontend::{Event, RuntimeFsm, RuntimeState, Session};
+    use super::{authentication, frontend, pre_startup};
+    use frontend::{Event, RuntimeFsm, RuntimeState, Session};
 
     #[test]
     fn generated_typestate_and_runtime_accept_the_extended_loop() {
@@ -96,5 +159,47 @@ mod tests {
         runtime.step(Event::Query).unwrap();
         runtime.step(Event::CopyIn).unwrap();
         assert!(runtime.step(Event::Query).is_err());
+    }
+
+    #[test]
+    fn generated_pre_startup_requires_handshake_before_startup() {
+        let _typed = pre_startup::Session::new()
+            .ssl_request()
+            .accept()
+            .complete()
+            .startup();
+
+        let mut runtime = pre_startup::RuntimeFsm::new();
+        runtime.step(pre_startup::Event::SslRequest).unwrap();
+        assert!(runtime.step(pre_startup::Event::Startup).is_err());
+        runtime.step(pre_startup::Event::Accept).unwrap();
+        runtime.step(pre_startup::Event::HandshakeComplete).unwrap();
+        runtime.step(pre_startup::Event::Startup).unwrap();
+    }
+
+    #[test]
+    fn generated_sasl_continuation_is_recursive() {
+        let _typed = authentication::Session::new()
+            .sasl()
+            .initial()
+            .continue_response()
+            .continue_response()
+            .final_response()
+            .ok()
+            .ready();
+
+        let mut runtime = authentication::RuntimeFsm::new();
+        for event in [
+            authentication::Event::Sasl,
+            authentication::Event::Initial,
+            authentication::Event::Continue,
+            authentication::Event::Continue,
+            authentication::Event::Final,
+            authentication::Event::Ok,
+            authentication::Event::Ready,
+        ] {
+            runtime.step(event).unwrap();
+        }
+        assert_eq!(runtime.state(), authentication::RuntimeState::Ready);
     }
 }

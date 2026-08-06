9e85a044583cfc8e61e0564f039d9a967f43f21e
James Sadler <freshtonic@gmail.com>
2026-08-06 23:09:47 +1000

    test: support platform-specific compiler diagnostics

--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -4,4 +4,9 @@
 fn illegal_protocol_transitions_do_not_compile() {
     let tests = trybuild::TestCases::new();
     tests.compile_fail("tests/ui/*.rs");
+    if cfg!(target_os = "macos") {
+        tests.compile_fail("tests/ui/platform/typed_outbound_conn_role_mismatch_macos.rs");
+    } else {
+        tests.compile_fail("tests/ui/platform/typed_outbound_conn_role_mismatch_linux.rs");
+    }
 }
